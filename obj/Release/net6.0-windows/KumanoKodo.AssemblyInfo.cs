94198080e97c5f9d61464329b2e9d8726190964e
lhovius <57228052+lhovius@users.noreply.github.com>
2025-03-19 18:43:25 +0100

    Dummy change

--- obj/Release/net6.0-windows/KumanoKodo.AssemblyInfo.cs
+++ obj/Release/net6.0-windows/KumanoKodo.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("KumanoKodo")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf7f6ef4cc5a2e764700112e7a6d2482f4efa08c")]
 [assembly: System.Reflection.AssemblyProductAttribute("KumanoKodo")]
